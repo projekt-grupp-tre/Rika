922b14d9726ea5a2aa860389bade068fecac3efa
Frida <fridapeersson@gmail.com>
2024-10-31 10:29:45 +0100

    get all products och productdetails working

--- RikaWebApp/Program.cs
+++ RikaWebApp/Program.cs
@@ -1,7 +1,20 @@
+
+using Business.Services.Product;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddHttpClient("AzureFunctionClient", client =>
+{
+    client.BaseAddress = new Uri("https://productprovidergraphql.azurewebsites.net/api/"); 
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+builder.Services.AddScoped<ProductService>();
+
 
 var app = builder.Build();
 
