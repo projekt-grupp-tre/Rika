a0fe754665645f169cf2679263f33f3f4d9d1e59
Edvin Thörnholm <edvin.thornholm@gmail.com>
2024-11-05 11:07:10 +0100

    pagnation done

--- RikaWebApp/wwwroot/js/product/product.js
+++ RikaWebApp/wwwroot/js/product/product.js
@@ -1,1 +1,28 @@
-﻿
+﻿document.querySelectorAll('.favorite-btn').forEach(button => {
+    button.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const icon = this.querySelector('i');
+
+        if (icon.classList.contains('fa-regular')) {
+            icon.classList.remove('fa-regular');
+            icon.classList.add('fa-solid');
+        } else {
+            icon.classList.remove('fa-solid');
+            icon.classList.add('fa-regular');
+        }
+    });
+});
+
+const searchToggle = document.getElementById('search-toggle');
+const searchBar = document.getElementById('search-bar');
+
+searchToggle.addEventListener('click', function () {
+    if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+        searchBar.style.display = 'block';
+        searchBar.querySelector('input').focus();
+    } else {
+        searchBar.style.display = 'none';
+    }
+});
+
