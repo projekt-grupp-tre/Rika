a06cf00da04872a6991d268b39c11f7ef0401cd1
Mara Neacsu <146334465+MaraNeacsu@users.noreply.github.com>
2024-11-06 09:24:07 +0100

    .

---
+++ RikaWebApp/Controllers/Auth/VerificationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RikaWebApp.Controllers.Auth
+{
+    public class VerificationController : Controller
+    {
+        public IActionResult VerificationView()
+        {
+            return View();
+        }
+    }
+}
