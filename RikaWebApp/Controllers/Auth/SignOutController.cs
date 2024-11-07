5951cdf33a04f965aa4af4c7b78717852c2661e3
Marcus <Marcuswix@yahoo.se>
2024-11-07 16:30:10 +0100

    "SignOut on Client Done".

---
+++ RikaWebApp/Controllers/Auth/SignOutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RikaWebApp.Controllers.Auth
+{
+    public class SignOutController : Controller
+    {
+        public IActionResult LogOut()
+        {
+            Response.Cookies.Delete("JwtToken");
+            TempData["LogOut"] = "You have successfully signed ut! Welcome back another time!";
+            return RedirectToAction("Home", "Index");
+        }
+    }
+}
