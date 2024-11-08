892323cfc7348c5f0666523f112dd2d93e365bf3
Levi Stark <levi.john.stark@gmail.com>
2024-11-08 10:24:39 +0100

    users can now chat with support agent client

--- RikaWebApp/Models/Communication/MessageModel.cs
+++ RikaWebApp/Models/Communication/MessageModel.cs
@@ -2,10 +2,12 @@
 {
     public class MessageModel
     {
+				public string UserName { get; set; } = null!;
         public string SenderUserId { get; set; } = null!;
-        public bool IsSentFromThisClient { get; set; }
-        public string Content { get; set; } = null!;
+				public string? GroupId { get; set; }
+        public string MessageContent { get; set; } = null!;
         public DateTime MessageSentAt = DateTime.Now;
         public DateTime? MessageReceivedAt {  get; set; }
+				public List<string>? AttachmentUrls { get; set; }
     }
 }
