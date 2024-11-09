d611eddfc7f4b64b29ed08eb30c8e26f629ada09
Gala Miteva <galina.miteva@utb.ecutbildning.se>
2024-11-09 20:05:46 +0100

    design

---
+++ RikaWebApp/wwwroot/js/auth/onboarding.js
@@ -0,0 +1,32 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    
+    if (window.location.href.includes("/onboarding")) {
+        onboardingFunction();
+    }
+});
+
+function onboardingFunction() {
+    const slides = document.querySelectorAll('.container .content');
+    let currentSlideIndex = 0;
+
+    if (slides.length > 0) {
+        slides[currentSlideIndex].style.display = 'block';
+
+        const buttons = document.querySelectorAll('#onboard-btn');
+
+        if (buttons.length > 0) {
+            buttons.forEach(button => {
+                button.addEventListener('click', () => {
+                    slides[currentSlideIndex].style.display = 'none';
+                    currentSlideIndex++;
+
+                    if (currentSlideIndex < slides.length) {
+                        slides[currentSlideIndex].style.display = 'block';
+                    } else {
+                        window.location.href = "https://localhost:7259/SignUp/SignUpView";
+                    }
+                });
+            });
+        }
+    }
+}
