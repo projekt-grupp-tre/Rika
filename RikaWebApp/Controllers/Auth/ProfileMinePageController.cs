3f014b8229f4c2432491f691f6666314b2c66748
Gala Miteva <galina.miteva@utb.ecutbildning.se>
2024-11-10 20:03:47 +0100

    profile-details exampel redo

---
+++ RikaWebApp/Controllers/Auth/ProfileMinePageController.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RikaWebApp.ViewModels;
+using System.Diagnostics;
+using System.Text;
+
+namespace RikaWebApp.Controllers.Auth;
+
+public class ProfileMinePageController(HttpClient http, IConfiguration configuration) : Controller
+{
+    private readonly HttpClient _http = http;
+    private readonly IConfiguration _configuration = configuration;
+
+    [HttpGet]
+    [Route("profile/details")]
+    public async Task<IActionResult> Details()
+    {
+        var viewModel = new ProfileUpdateViewModel();
+       
+
+        // en  userId av UsersProvider_Rika
+       // string userId = "aaaaaaaa-2dbf-4f6d-aa18-2fff495a9c1c";  // en  userId av UserProvider_Rika
+        // string userId = "dddddddd-a9e1-4694-9a5d-3c8a4b58fdba";
+        // string userId = "eeeeeeee-61b0-4210-bdb5-ccda889c832d";
+
+
+
+        //tests
+         string userId = "00000000-f85d-429c-96f9-736696f6b190";
+        // string userId = "cccccccc-abb5-4421-ad64-3635f31a6233";
+        // string userId = "ffffffff-4663-b8e9-e2accb5ce188";
+
+
+
+
+        try
+        {
+            var requestUrl = $"http://localhost:7074/api/users/{userId}";
+            var response = await _http.GetAsync(requestUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                viewModel = JsonConvert.DeserializeObject<ProfileUpdateViewModel>(await response.Content.ReadAsStringAsync());
+                return View(viewModel);
+            }
+            else
+            {
+                Debug.WriteLine("ERROR :: Request failed - " + response.StatusCode);
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("ERROR :: " + ex.Message);
+        }
+
+        return View(viewModel);
+    }
+
+
+
+
+
+    [HttpPost]
+    public async Task<IActionResult> Details(ProfileUpdateViewModel detailsViewModel)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                // en  userId av UsersProvider_Rika
+                string userId = "00000000-f85d-429c-96f9-736696f6b190";
+
+                
+                var updateModel = new ProfileUpdateViewModel
+                {
+                    FirstName = detailsViewModel.FirstName,
+                    LastName = detailsViewModel.LastName,
+                    Email = detailsViewModel.Email,
+                    Address = detailsViewModel.Address,
+                    City = detailsViewModel.City,
+                    PostalCode = detailsViewModel.PostalCode,
+                    Country = detailsViewModel.Country,
+                    ImageUrl = detailsViewModel.ImageUrl
+                };
+
+               
+                var json = new StringContent(JsonConvert.SerializeObject(updateModel), Encoding.UTF8, "application/json");
+
+              
+                var requestUrl = $"http://localhost:7074/api/users/{userId}";
+                var response = await _http.PostAsync(requestUrl, json);
+
+               
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Details");
+                }
+                else
+                {
+                    Debug.WriteLine("ERROR :: Request failed - " + response.StatusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("ERROR :: " + ex.Message);
+            }
+        }
+
+       
+        return View(detailsViewModel);
+    }
+
+
+
+
+    // delete
+
+    public async Task<IActionResult> DeleteAccount()
+    {
+        try
+        {
+
+            // en  userId av UsersProvider_Rika
+            // string userId = "aaaaaaaa-2dbf-4f6d-aa18-2fff495a9c1c";
+
+
+            // string userId = "dddddddd-a9e1-4694-9a5d-3c8a4b58fdba";
+            // string userId = "eeeeeeee-61b0-4210-bdb5-ccda889c832d";
+
+
+
+            //tests
+            string userId = "00000000-f85d-429c-96f9-736696f6b190";
+            // string userId = "cccccccc-abb5-4421-ad64-3635f31a6233";
+            // string userId = "ffffffff-4663-b8e9-e2accb5ce188";
+
+
+            var requestUrl = $"http://localhost:7074/api/users/{userId}";
+
+          
+            var response = await _http.DeleteAsync(requestUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Home"); 
+            }
+            else
+            {
+                Debug.WriteLine("ERROR :: Bad request - " + response.StatusCode);
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("ERROR :: " + ex.Message);
+        }
+
+        return RedirectToAction("Index");
+    }
+
+    
+}
---
+++ RikaWebApp/ViewModels/ProfileUpdateViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RikaWebApp.ViewModels;
+
+public class ProfileUpdateViewModel
+{
+    [DataType(DataType.Text)]
+    [Display(Name = "First name", Prompt = "First name", Order = 0)]
+    [Required(ErrorMessage = "Invalid first name")]
+    [MinLength(2, ErrorMessage = "Invalid first name")]
+    public string FirstName { get; set; } = null!;
+
+    [DataType(DataType.Text)]
+    [Display(Name = "Last name", Prompt = "Last name", Order = 1)]
+    [Required(ErrorMessage = "Invalid last name")]
+    [MinLength(2, ErrorMessage = "Invalid last name")]
+    public string LastName { get; set; } = null!;
+
+    [Display(Name = "Email address", Prompt = "Email address", Order = 2)]
+    [DataType(DataType.EmailAddress)]
+    [Required(ErrorMessage = "Invalid email address")]
+    [RegularExpression("^[^@\\s]+@[^@\\s]+\\.[^@\\s]{2,}$", ErrorMessage = "invalid email")]
+    public string Email { get; set; } = null!;
+
+
+    [DataType(DataType.Text)]
+    [Display(Name = "Address", Prompt = "Street name", Order = 3)]
+    [Required(ErrorMessage = "Invalid address")]
+    [MinLength(2, ErrorMessage = "Invalid address")]
+    public string Address { get; set; } = null!;
+
+    [DataType(DataType.Text)]
+    [Display(Name = "City", Prompt = "City", Order = 4)]
+    [Required(ErrorMessage = "Invalid City name")]
+    [MinLength(2, ErrorMessage = "Invalid City name")]
+    public string City { get; set; } = null!;
+
+
+    [DataType(DataType.Text)]
+    [Display(Name = "PostalCode", Prompt = "PostalCode", Order = 5)]
+    [Required(ErrorMessage = "Invalid PostalCode")]
+    [MinLength(5, ErrorMessage = "Invalid PostalCode")]
+    public string PostalCode { get; set; } = null!;
+
+    [DataType(DataType.Text)]
+    [Display(Name = "Country", Prompt = "Country", Order = 6)]
+    [Required(ErrorMessage = "Invalid Country")]
+    [MinLength(3, ErrorMessage = "Invalid Country")]
+    public string Country { get; set; } = null!;
+
+
+    [DataType(DataType.Text)]
+    [Display(Name = "Age", Prompt = "Age", Order = 7)]
+    [Required(ErrorMessage = "Invalid Age")]
+    [MinLength(3, ErrorMessage = "Invalid Age")]
+    public string Age { get; set; } = null!;
+
+
+
+
+    [DataType(DataType.Text)]
+    [Display(Name = "ImageUrl", Prompt = "Profile Image", Order = 8)]
+    [Required(ErrorMessage = "Invalid Profile ImageUrl")]
+    [MinLength(2, ErrorMessage = "Invalid Profile ImageUrl")]
+    public string ImageUrl { get; set; } = null!;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+    
